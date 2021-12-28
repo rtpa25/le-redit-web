ca95de89ba03a2f95e4e0fbcf8f407ccedca9f84
Ronit Panda <pandaronit25@gmail.com>
2021-12-28 12:53:05 +0530

    deploy

--- utils/betterUpdateQuery.ts
+++ utils/betterUpdateQuery.ts
@@ -8,5 +8,5 @@ export function betterUpdateQuery<Result, Query>(
   result: any,
   fn: (r: Result, q: Query) => Query
 ) {
-  return cache.updateQuery(qi, (data: Query) => fn(result, data));
+  return (cache as any).updateQuery(qi, (data: Query) => fn(result, data));
 }
