847571812a664d6fc9e445474400ac6bdb8c973c
Ronit Panda <pandaronit25@gmail.com>
2022-01-07 12:33:50 +0530

    fixed cors issue

--- config/appinfo.ts
+++ config/appinfo.ts
@@ -1,8 +1,14 @@
 /** @format */
 
+import { __isProd__ } from '../utils/types';
+
 export const appInfo = {
   // learn more about this on https://supertokens.io/docs/emailpassword/appinfo
   appName: 'ether',
-  apiDomain: 'http://localhost:4000',
-  websiteDomain: 'http://localhost:3000',
+  apiDomain: __isProd__
+    ? 'https://api.etherapp.social'
+    : 'http://localhost:4000',
+  websiteDomain: __isProd__
+    ? 'https://www.etherapp.social'
+    : 'http://localhost:3000',
 };
